6a24cc47fc53d17b77ecd9b45862265329039832
Mikhail Fedotov <fedotov@rutoken.ru>
2021-02-21 01:33:31 +0300

    Add object states negative test

---
+++ kstatemachine/src/test/kotlin/ru/nsk/kstatemachine/ObjectStatesTest.kt
@@ -0,0 +1,36 @@
+package ru.nsk.kstatemachine
+
+import io.kotest.assertions.throwables.shouldThrow
+import org.junit.jupiter.api.Test
+import java.lang.IllegalStateException
+
+private object State1 : DefaultState("state1")
+private object State2 : DefaultState("state2")
+
+/**
+ * States are mutable and currently it is not possible to use object states in multiple [StateMachine] instances.
+ * May be it might be partly fixed with moving mutated state part into [StateMachine] (some graph object),
+ * but it will not completely protect from wrong usage (for example [State] subclasses may hold some mutable data).
+ */
+class ObjectStatesTest {
+    @Test
+    fun multipleUsageOfObjectStates() {
+        useInMachine()
+        shouldThrow<IllegalStateException> {
+            useInMachine()
+        }
+    }
+
+    private fun useInMachine() {
+        val machine = createStateMachine {
+            addInitialState(State1) {
+                transition<SwitchEvent> {
+                    targetState = State2
+                }
+            }
+            addState(State2)
+        }
+
+        machine.processEvent(SwitchEvent)
+    }
+}
