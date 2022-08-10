e469db5df3a215be0d7ed410886fe740da523a87
Mikhail Fedotov <nosik90@gmail.com>
2022-08-10 23:17:18 +0300

    [version] Up version to 0.13.0

--- buildSrc/src/main/kotlin/ru/nsk/Versions.kt
+++ buildSrc/src/main/kotlin/ru/nsk/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     // library
     const val libraryGroup = "ru.nsk"
-    const val libraryVersion = "0.12.0"
+    const val libraryVersion = "0.13.0"
 
     // tools
     const val kotlin = "1.7.10"
