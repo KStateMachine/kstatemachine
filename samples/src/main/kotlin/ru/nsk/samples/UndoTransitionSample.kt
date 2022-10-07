8b5a3e5c7e9922fbf889979ef98b4383b071e775
Mikhail Fedotov <nosik90@gmail.com>
2022-10-07 11:19:25 +0300

    [docs] Fix links

--- samples/src/main/kotlin/ru/nsk/samples/UndoTransitionSample.kt
+++ samples/src/main/kotlin/ru/nsk/samples/UndoTransitionSample.kt
@@ -13,17 +13,29 @@ private object UndoTransitionSample {
 fun main() {
     lateinit var state1: State
     lateinit var state2: State
+    lateinit var state3: State
 
     val machine = createStateMachine(enableUndo = true) {
         state1 = initialState("state1") {
             transitionOn<SwitchEvent> { targetState = { state2 } }
         }
-        state2 = state("state2")
+        state2 = state("state2") {
+            onEntry {
+                println("$this entered with event ${it.event}, ${it.unwrappedEvent}")
+                check(it.unwrappedEvent is SwitchEvent)
+            }
+
+            transitionOn<SwitchEvent> { targetState = { state3 } }
+        }
+        state3 = state("state3") {
+            onEntry { machine.undo() }
+        }
     }
 
     machine.processEvent(SwitchEvent)
-    check(state2 in machine.activeStates())
+    machine.processEvent(SwitchEvent)
 
     machine.undo() // same as machine.processEvent(UndoEvent)
+
     check(state1 in machine.activeStates())
 }
