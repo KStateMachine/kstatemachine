6b9d37661b4b77655729f9e15ff3107126095f76
Mikhail Fedotov <nosik90@gmail.com>
2025-08-12 22:13:51 +0300

    [version] Upgrade version to 0.34.2

--- buildSrc/src/main/kotlin/ru/nsk/Versions.kt
+++ buildSrc/src/main/kotlin/ru/nsk/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     // library
     const val libraryMavenCentralGroup = "io.github.nsk90"
     const val libraryJitPackGroup = "com.github.nsk90"
-    const val libraryVersion = "0.34.1"
+    const val libraryVersion = "0.34.2"
 
     // tools
     const val kotlin = "2.2.0"
